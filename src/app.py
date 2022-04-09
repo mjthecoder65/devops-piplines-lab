38a6af19a601b304e104fab7c318994d9da36b3f
Michael Jordan <mjthecoder53161@gmail.com>
2022-04-09 14:25:07 +0900

    Implemented new features

--- src/app.py
+++ src/app.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, abort, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+books = [
+    {"id": 1, "title": "The subtle art of not giving a fuck", "author": "Mark Manson"},
+    {"id": 2, "title": "Cracking Coding Interview", "author": "Dayle Laakmann McDowell"},
+    {"id": 3, "title": "How not to die", "author": "Michael Greger" },
+    {"id": 4, "title": "Meditation and Mindfulness", "author": "Andy Puddicombe"},
+    {"id": 5, "title": "The Monk Who Sold His Ferrari", "author": "Robin Sharma"}
+]
+
+@app.route('/', methods=["GET"])
 def index():
     return { "id": 1, "message": "Welcome to hands-on DevOps Workshop"}
+
+@app.route("/api/v1/books", methods=["GET"])
+def get_books():
+    return { "books": books }
+
+@app.route("/api/v1/books/<int:id>", methods=["GET"])
+def get_book(id: int):
+    for book in books:
+        if book["id"] == id:
+            return book
+    return abort(404) 
+    
+
+# @app.route("/api/v1/books/", methods=["POST"])
+# def add_book():
+#     book = dict(request.get_json(force=True))
+#     book["id"] = len(books) + 1
+#     books.append(book)
+#     return book
+
+@app.route("/api/v1/books/<int:id>", methods=["DELETE"])
+def delete_book(id: int):
+    for index, book in enumerate(books):
+        if book["id"] == id:
+            del books[index]
+            return book
+    return abort(404)
+
 
 if __name__ == '__main__':
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,46 @@
-from app import index
+from app import app, books
 
+client = app.test_client()
 
 def test_index():
-    res = index()
-    assert res['id'] == 1
+    res = client.get('/')
+    assert res.status_code == 200
+
+
+def test_get_books():
+    res = client.get('/api/v1/books')
+    data = res.json
+    assert res.status_code == 200
+    assert len(data['books']) == len(books)
+
+
+def test_get_book_return_200_Ok():
+    res = client.get("/api/v1/books/1")
+    data = res.json
+    assert res.status_code == 200
+    assert data['author'] == "Mark Manson"
+
+
+def test_get_book_return_404():
+    res = client.get("/api/v1/books/1000")
+    assert res.status_code == 404
+
+# def test_add_book():
+#     count = len(books)
+#     book = { "title": "Computer Science Unleashed", "author": "Wlaston Ferreira" }
+#     res = client.post("/api/v1/books", json=book)
+#     assert count < len(books)
+
+def test_delete_return_200_ok():
+    count = len(books)
+    res = client.delete("/api/v1/books/1")
+
+    assert res.json["id"] == 1
+    assert len(books) < count
+
+def test_delete_return_404():
+    res = client.delete("/api/v1/books/100000")
+    assert res.status_code == 404
+
+
+
